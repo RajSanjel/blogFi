6692ce3535ca9cb65d572f384ffd5425127b7ade
Ardy Pranata <ardiprana90@gmail.com>
2024-06-10 20:13:22 +0700

    style: handle larger media screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 // import bgImage from "./images/finance.jpg"
 
-
 const Header = () => {
-    return (
-        <div className={`h-[31rem] bg-white w-full p-20 bg-[url('/src/components/images/bg.jpg')] bg-no-repeat bg-right bg-contain font-semibold flex items-center`}>
-            <h1 className="text-5xl md:text-7xl">
-                Finance <br />
-                Stories & Tips
-                <i className="text-sm block">By Experts</i>
-            </h1>
-
-        </div>
-    );
-}
+  return (
+    <div className="md:max-w-[1600px] mx-auto p-20">
+      <div className="flex items-center h-[31rem] w-full font-semibold bg-[url('/src/components/images/bg.jpg')] bg-no-repeat bg-right bg-contain">
+        <h1 className="text-5xl md:text-7xl pr-6">
+          Finance <br />
+          Stories & Tips
+          <i className="text-sm block">By Experts</i>
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
